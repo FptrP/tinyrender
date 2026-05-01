c09f02f165c7ffd1bcd458166e69d2005555ef40
yyyy <fptr1914@gmail.com>
2026-05-02 01:55:52 +0300

    Swapchain recreation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::sync::Arc;
 
 use winit::{
-    application::ApplicationHandler, event::WindowEvent, event_loop::EventLoop, window::Window
+    application::ApplicationHandler, dpi::{PhysicalSize, Size}, event::WindowEvent, event_loop::EventLoop, window::{Window, WindowAttributes}
 };
 
 use crate::render::Render;
@@ -35,8 +35,16 @@ impl ApplicationHandler for App {
                 // todo: draw logic
                 //
                 let render = self.render.as_mut().unwrap();
+                let window = self.window.as_ref().unwrap();
+
                 render.draw_frame();
                 
+                if render.recreate_swapchain {
+                    let res = [window.inner_size().width,
+                        window.inner_size().height];
+                    render.recreate_swapchain(res);
+                }
+                //render.recreate_swapchain()
                 self.window.as_ref().unwrap().request_redraw();
             },
             _ => {},
@@ -44,7 +52,11 @@ impl ApplicationHandler for App {
     }
 
     fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
-        self.window = Some(Arc::new(event_loop.create_window(Window::default_attributes()).unwrap()));
+
+        let winfo = Window::default_attributes()
+            .with_inner_size(PhysicalSize::new(640, 480));
+
+        self.window = Some(Arc::new(event_loop.create_window(winfo).unwrap()));
         
         let resolution = self.window.as_ref().unwrap().inner_size();
         let vkstate = vkstate::State::new_for_rendering(self.window.as_ref().unwrap().clone(), [resolution.width, resolution.height]);
--- src/render.rs
+++ src/render.rs
@@ -1,7 +1,7 @@
 
 use std::sync::Arc;
 
-use vulkano::{Validated, command_buffer::{AutoCommandBufferBuilder, RenderPassBeginInfo, SubpassBeginInfo, SubpassEndInfo, allocator::{CommandBufferAllocator, StandardCommandBufferAllocator}}, device::Device, format::Format, image::{ImageAspects, ImageLayout}, swapchain::SwapchainPresentInfo, sync::{AccessFlags, GpuFuture, PipelineStages, future::FenceSignalFuture}};
+use vulkano::{Validated, command_buffer::{AutoCommandBufferBuilder, RenderPassBeginInfo, SubpassBeginInfo, SubpassEndInfo, allocator::{CommandBufferAllocator, StandardCommandBufferAllocator}}, device::Device, format::Format, image::{ImageAspects, ImageLayout}, swapchain::{SwapchainCreateInfo, SwapchainPresentInfo}, sync::{AccessFlags, GpuFuture, PipelineStages, future::FenceSignalFuture}};
 
 
 use vulkano::render_pass::{SubpassDescription, SubpassDependency, AttachmentDescription, AttachmentReference, RenderPass, RenderPassCreateFlags, Framebuffer, RenderPassCreateInfo};
@@ -10,6 +10,7 @@ use vulkano::render_pass::FramebufferCreateInfo;
 use vulkano::image::view::{ImageView, ImageViewCreateInfo};
 
 use vulkano::swapchain::AcquireNextImageInfo;
+use vulkano::VulkanError;
 
 use crate::vkstate;
 
@@ -25,6 +26,7 @@ pub struct Render
     framebuffers : Vec<Arc<Framebuffer>>,
     frames_in_flight : Vec<Option<Arc<FenceSignalFuture<Box<dyn GpuFuture>>>>>,
     frame_index : usize,
+    pub recreate_swapchain : bool,
 }
 
 impl Render {
@@ -101,16 +103,32 @@ impl Render {
             cmd_allocator,
             frames_in_flight : [None, None, None].into(),
             frame_index : 0,
+            recreate_swapchain : false,
         }
 
     }
 
     pub fn draw_frame(&mut self) {
+        if self.recreate_swapchain {
+            return;
+        }
+
         let swapchain = self.vkstate.swapchain.as_ref().unwrap();     
         
-        let (backbuffer_id, _, acquire_future) = vulkano::swapchain::acquire_next_image(swapchain.clone(), None)
-            .map_err(Validated::unwrap).unwrap();
+        let (backbuffer_id, suboptimal, acquire_future) = 
+            match vulkano::swapchain::acquire_next_image(swapchain.clone(), None)
+            .map_err(Validated::unwrap)
+        {
+            Ok(v) => v,
+            Err(VulkanError::OutOfDate) => {
+                self.recreate_swapchain = true;
+                return;
+            },
+            Err(e) => panic!("{}", e),
+        };
         
+        self.recreate_swapchain |= suboptimal;
+
         let mut cmd_builder = AutoCommandBufferBuilder::primary(self.cmd_allocator.clone(), self.vkstate.main_queue_family, 
             vulkano::command_buffer::CommandBufferUsage::OneTimeSubmit).unwrap();
     
@@ -143,17 +161,53 @@ impl Render {
             Some(prev_fence) => prev_fence.clone().boxed()
         };
         
-        let future = previous_future
+        let submit = previous_future
             .join(acquire_future)
             .then_execute(self.vkstate.main_queue.clone(), cmd)
             .unwrap()
             .then_swapchain_present(self.vkstate.main_queue.clone(), SwapchainPresentInfo::swapchain_image_index(swapchain.clone(), backbuffer_id))
             .boxed()
             .then_signal_fence_and_flush()
-            .unwrap();
+            .map_err(Validated::unwrap);
+        
+        let future = match submit {
+            Ok(f) => Some(Arc::new(f)),
+            Err(VulkanError::OutOfDate) => {
+                self.recreate_swapchain = true;
+                None
+            },
+            Err(e) => panic!("{}", e),
+        };
 
-        self.frames_in_flight[self.frame_index] = Some(Arc::new(future));
+        self.frames_in_flight[self.frame_index] = future;
         self.frame_index = (self.frame_index + 1) % self.frames_in_flight.len();
+    }
+
+    pub fn recreate_swapchain(&mut self, new_extent : [u32; 2]) {
+        let swapchain_old = self.vkstate.swapchain.as_ref().unwrap();
+        let resized = new_extent != swapchain_old.image_extent();
+
+        if !resized && !self.recreate_swapchain {
+            return;
+        }
+        println!("[render] recreate swapchain {}x{} -> {}x{}",
+            swapchain_old.image_extent()[0],
+            swapchain_old.image_extent()[1],
+            new_extent[0],
+            new_extent[1]);
+
+        let (swapchain, images) = swapchain_old.recreate(
+            SwapchainCreateInfo {
+                image_extent : new_extent,
+              ..swapchain_old.create_info()
+            }).unwrap();
+
+        self.vkstate.swapchain = Some(swapchain);
+        self.vkstate.backbuffers = images;
+
+        self.framebuffers = Self::create_framebuffers(&self.vkstate, 
+            &self.main_renderpass);
+        self.recreate_swapchain = false;
     }
 
 }
